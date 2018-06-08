686ac4680afd2d75170bb795be9bd8510cf0a4bf
Edward Dupre <edwd42@gmail.com>
2018-06-08 10:13:50 -0400

    initial commit

---
+++ src/main/java/net/ed/scraper/AwsSpringSeleniumMysqlScraperApplication.java
@@ -0,0 +1,12 @@
+package net.ed.scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringSeleniumMysqlScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringSeleniumMysqlScraperApplication.class, args);
+	}
+}
---
+++ src/test/java/net/ed/scraper/AwsSpringSeleniumMysqlScraperApplicationTests.java
@@ -0,0 +1,16 @@
+package net.ed.scraper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsSpringSeleniumMysqlScraperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
